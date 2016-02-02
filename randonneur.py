19d15e3ef4548b593a3d9139638191a2e3dc440f
etrou <emmanuel.trouve@univ-savoie.fr>
2016-02-02 15:33:00 +0100

    ajout code plot

--- randonneur.py
+++ randonneur.py
@@ -19,3 +19,15 @@ def mafonction(t, omega = 1., tau = 10.):
 t = np.linspace(0., 10., 1000)
 omega = range(10) # valeur de Omega
 a = mafonction(t)
+
+plt.figure("Notre figure")
+plt.clf() # purge la figure
+plt.plot(t,a, color="b", label = "$\omega = {0}$".format(omega[1]))
+# plt.plot(t,a, marker = "+", color="m", linewidth=3, linestyle="--")
+# plt.plot(t,a, "*c--" linewidth=3) 
+plt.legend()
+plt.grid()
+plt.xlabel("Temps $t$ [s]")
+plt.ylabel("Amplitude, $a$, [N]")
+plt.show()
+plt.savefig("mafigure.pdf")

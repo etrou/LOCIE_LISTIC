c0b11f0a83499973d3b26cdbb692d258adf99d85
schuchag <gui.schuchardt@hotmail.fr>
2016-02-02 15:23:34 +0100

    Sinusoide

--- randonneur.py
+++ randonneur.py
@@ -6,9 +6,16 @@ Created on Tue Feb 02 14:55:54 2016
 """
 
 
-
+import numpy as np
 import matplotlib.pyplot as plt
 
+def mafonction(t, omega = 1., tau = 10.):
+    
+   
+    return np.exp(-t / tau) * np.sin(omega * t) #carré de x (**)
 
-def (x, y):
-    return 
+    #[] correspond à une liste 
+    
+t = np.linspace(0., 10., 1000)
+omega = range(10) # valeur de Omega
+a = mafonction(t)
